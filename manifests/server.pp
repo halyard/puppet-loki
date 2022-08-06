546ff7ae7c035eeff77971bf301530b58f77b459
Les Aker <me@lesaker.org>
2022-08-06 19:04:47 -0400

    add credentials

--- manifests/server.pp
+++ manifests/server.pp
@@ -2,10 +2,14 @@
 #
 # @param hostname is the hostname for log submission
 # @param tls_account is the account details for requesting the TLS cert
+# @param grafana_password is the password for grafana to access logs
+# @param promtail_password is the password for promtail to submit logs
 # @param tls_challengealias is the domain to use for TLS cert validation
 class loki::server (
   String $hostname,
   String $tls_account,
+  String $grafana_password,
+  String $promtail_password,
   Optional[String] $tls_challengealias = undef,
 ) {
   package { ['loki', 'logcli']: }
@@ -29,5 +33,9 @@ class loki::server (
     proxy_target       => 'http://localhost:3100',
     tls_challengealias => $tls_challengealias,
     tls_account        => $tls_account,
+    users              => {
+      'grafana'  => $grafana_password,
+      'promtail' => $promtail_password,
+    }
   }
 }
