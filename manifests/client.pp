e9900e1751143ed3924928ef9b792bb29eacd3d2
Les Aker <me@lesaker.org>
2022-08-11 19:44:00 -0400

    add syslog

--- manifests/client.pp
+++ manifests/client.pp
@@ -3,10 +3,12 @@
 # @param server sets the server address for Loki
 # @param password sets the promtail user password to submit logs
 # @param logpaths sets additional directories to pull logs from
+# @param syslog_sources sets IPs to expect syslog data from
 class loki::client (
   String $server,
   String $password,
   Hash[String, String] $logpaths = {},
+  Array[String] $syslog_sources = [],
 ) {
   package { 'promtail': }
 
@@ -18,5 +20,20 @@ class loki::client (
   ~> service { 'promtail':
     ensure => running,
     enable => true,
+  }
+
+  $syslog_sources.each |String $source| {
+    firewall { "100 allow syslog tcp input from ${source}":
+      dport  => 1514,
+      source => $source,
+      proto  => 'tcp',
+      action => 'accept',
+    }
+    firewall { "100 allow syslog udp input from ${source}":
+      dport  => 1514,
+      source => $source,
+      proto  => 'udp',
+      action => 'accept',
+    }
   }
 }
