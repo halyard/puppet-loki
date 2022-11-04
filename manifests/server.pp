1ad6993698b352cfa63debc5fd51a58f99f8c80e
Les Aker <me@lesaker.org>
2022-11-03 23:42:29 -0400

    add backups

--- manifests/server.pp
+++ manifests/server.pp
@@ -5,12 +5,22 @@
 # @param grafana_password is the password for grafana to access logs
 # @param promtail_password is the password for promtail to submit logs
 # @param tls_challengealias is the domain to use for TLS cert validation
+# @param backup_target sets the target repo for backups
+# @param backup_watchdog sets the watchdog URL to confirm backups are working
+# @param backup_password sets the encryption key for backup snapshots
+# @param backup_environment sets the env vars to use for backups
+# @param backup_rclone sets the config for an rclone backend
 class loki::server (
   String $hostname,
   String $tls_account,
   String $grafana_password,
   String $promtail_password,
   Optional[String] $tls_challengealias = undef,
+  Optional[String] $backup_target = undef,
+  Optional[String] $backup_watchdog = undef,
+  Optional[String] $backup_password = undef,
+  Optional[Hash[String, String]] $backup_environment = undef,
+  Optional[String] $backup_rclone = undef,
 ) {
   package { ['loki', 'logcli']: }
 
@@ -37,5 +47,16 @@ class loki::server (
       'grafana'  => $grafana_password,
       'promtail' => $promtail_password,
     },
+  }
+
+  if $backup_target != '' {
+    backup::repo { 'loki':
+      source         => '/var/lib/loki',
+      target         => $backup_target,
+      watchdog_url   => $backup_watchdog,
+      password       => $backup_password,
+      environment    => $backup_environment,
+      rclone_options => $backup_rclone,
+    }
   }
 }
