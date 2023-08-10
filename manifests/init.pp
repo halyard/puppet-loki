87d9c81267ebeb479f1c02578b0ef54812687d5b
Les Aker <me@lesaker.org>
2023-08-09 22:43:23 -0400

    break key apart

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,13 +2,15 @@
 class loki (
 ) {
   if $facts['os']['family'] =~ /(Debian|Ubuntu)/ {
-    apt::source { 'loki':
+    apt::key { 'loki':
+      id     => '0E22EB88E39E12277A7760AE9E439B102CF3C0C6',
+      source => 'https://apt.grafana.com/gpg-full.key',
+    }
+
+    -> apt::source { 'loki':
       location => 'https://apt.grafana.com',
-      repos    => 'stable main',
-      key      => {
-        'id'     => '0E22EB88E39E12277A7760AE9E439B102CF3C0C6',
-        'source' => 'https://apt.grafana.com/gpg-full.key',
-      },
+      release  => 'stable',
+      repos    => 'main',
     }
   }
 }
